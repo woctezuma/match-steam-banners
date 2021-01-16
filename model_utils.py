62b85cc9d3642fec37cde70eeb933d84676a5d8a
Wok <woctezuma@users.noreply.github.com>
2021-01-16 17:23:04 +0100

    Add a separate function get_input_shape()

--- model_utils.py
+++ model_utils.py
@@ -13,17 +13,20 @@ def get_target_model_size(resolution=None):
     return target_model_size
 
 
+def get_input_shape(target_model_size, num_channels=3):
+    # Image data format: channels last
+    input_shape = tuple(list(target_model_size) + [num_channels])
+
+    return input_shape
+
+
 def load_keras_model(target_model_size=None, include_top=False, pooling="avg"):
     # Reference: https://github.com/keras-team/keras-applications/blob/master/keras_applications/mobilenet_v3.py
 
     if target_model_size is None:
         target_model_size = get_target_model_size()
 
-    num_channels = 3
-
-    # Image data format: channels last
-    input_shape = tuple(list(target_model_size) + [num_channels])
-
+    input_shape = get_input_shape(target_model_size)
     model = get_model(input_shape=input_shape, include_top=include_top, pooling=pooling)
 
     return model
