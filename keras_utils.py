b0c3f7af8bc3f123b3d8e83ea3d9741b2ed9d0aa
Wok <woctezuma@users.noreply.github.com>
2021-01-16 17:29:17 +0100

    Minor refactoring: move function label_image()

--- keras_utils.py
+++ keras_utils.py
@@ -1,5 +1,8 @@
+import numpy as np
 from tensorflow.keras.applications import MobileNetV3Small
 from tensorflow.keras.applications.mobilenet_v3 import preprocess_input
+
+from generic_utils import convert_image_to_array
 
 
 def get_model_resolution():
@@ -15,3 +18,21 @@ def preprocess_image_array_for_model(image_array):
 def get_model(input_shape, include_top=False, pooling="avg"):
     model = MobileNetV3Small(input_shape=input_shape, include_top=include_top, pooling=pooling)
     return model
+
+
+def label_image(image, model):
+    # Reference: https://github.com/glouppe/blackbelt/
+
+    # convert the image pixels to a numpy array
+    image = convert_image_to_array(image)
+
+    # reshape data for the model
+    image = np.expand_dims(image, axis=0)
+
+    # prepare the image for the VGG model
+    image = preprocess_image_array_for_model(image)
+
+    # predict the probability across all output classes
+    yhat = model.predict(image)
+
+    return yhat
--- model_utils.py
+++ model_utils.py
@@ -1,7 +1,4 @@
-import numpy as np
-
-from generic_utils import convert_image_to_array
-from keras_utils import get_model, preprocess_image_array_for_model, get_model_resolution
+from keras_utils import get_model, get_model_resolution, label_image
 
 
 def get_target_model_size(resolution=None):
@@ -30,24 +27,6 @@ def load_keras_model(target_model_size=None, include_top=False, pooling="avg"):
     model = get_model(input_shape=input_shape, include_top=include_top, pooling=pooling)
 
     return model
-
-
-def label_image(image, model):
-    # Reference: https://github.com/glouppe/blackbelt/
-
-    # convert the image pixels to a numpy array
-    image = convert_image_to_array(image)
-
-    # reshape data for the model
-    image = np.expand_dims(image, axis=0)
-
-    # prepare the image for the VGG model
-    image = preprocess_image_array_for_model(image)
-
-    # predict the probability across all output classes
-    yhat = model.predict(image)
-
-    return yhat
 
 
 def convert_image_to_features(image, model):
