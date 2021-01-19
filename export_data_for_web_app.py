5161b9443ad49733d08e671e26b43a1dc9ef622f
Wok <woctezuma@users.noreply.github.com>
2021-01-19 19:51:13 +0100

    Export data and results for a web app

---
+++ export_data_for_web_app.py
@@ -0,0 +1,94 @@
+import json
+from pathlib import Path
+
+import numpy as np
+
+from app_id_utils import load_frozen_app_ids
+from data_utils import get_label_database_filename
+from faiss_utils import get_faiss_search_structure, find_knn_for_all
+
+
+def get_export_folder_name():
+    export_folder_name = "data_export/"
+    Path(export_folder_name).mkdir(exist_ok=True)
+
+    return export_folder_name
+
+
+def export_app_ids(out_fname_json="app_ids.json", out_fname_npy="app_ids.npy"):
+    app_ids = [int(app_id) for app_id in load_frozen_app_ids()]
+
+    print("#apps = {}".format(len(app_ids)))
+
+    with open(get_export_folder_name() + out_fname_json, "w", encoding="utf8") as f:
+        json.dump(app_ids, f)
+
+    # As of January 2021, the largest appID on Steam is less than 2 millions, so uint32 is ok (max value: ~ 4.2 billion)
+    v = np.asarray(app_ids, dtype=np.uint32)
+
+    np.save(
+        get_export_folder_name() + out_fname_npy,
+        v,
+        allow_pickle=False,
+        fix_imports=False,
+    )
+
+    return app_ids
+
+
+def export_app_names(
+    app_ids, input_fname="IStoreService.json", out_fname="app_names.json"
+):
+    with open(input_fname, "r", encoding="utf8") as f:
+        data = json.load(f)
+
+    app_info = dict()
+    for e in data["response"]["apps"]:
+        app_info[e["appid"]] = e["name"]
+
+    app_names = []
+    for app_id in app_ids:
+        app_name = app_info[int(app_id)]
+        app_names.append(app_name)
+
+    print("#apps = {}".format(len(app_names)))
+
+    with open(get_export_folder_name() + out_fname, "w") as f:
+        json.dump(app_names, f)
+
+    return app_names
+
+
+def export_matches(
+    num_neighbors=100, use_cosine_similarity=True, out_fname="matches_faiss.npy"
+):
+    embeddings = np.load(get_label_database_filename())
+
+    print("(#apps, #features) = {}".format(embeddings.shape))
+
+    index = get_faiss_search_structure(
+        embeddings, use_cosine_similarity=use_cosine_similarity
+    )
+
+    D, I = find_knn_for_all(
+        index, embeddings, num_neighbors, use_cosine_similarity=use_cosine_similarity
+    )
+
+    # As of January 2021, the largest list index is about ~ 30 k (#apps), so uint16 is ok (max value: ~ 65 k)
+    v = I.astype("uint16")
+
+    np.save(
+        get_export_folder_name() + out_fname, v, allow_pickle=False, fix_imports=False
+    )
+
+    return
+
+
+if __name__ == "__main__":
+    app_ids = export_app_ids()
+
+    input_fname = "IStoreService.json"
+    if Path(input_fname).exists():
+        app_names = export_app_names(app_ids, input_fname=input_fname)
+
+    export_matches(num_neighbors=100, use_cosine_similarity=True)
